1efa09f67ecbe7afe90011ccedb97bf20d85b2b1
= <alejandro.rivas@go.tarleton.edu>
2021-09-14 08:28:24 -0500

    Initial commit

---
+++ DataStructures/src/main/java/com/mycompany/datastructures/Queue.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.datastructures;
+
+
+public class Queue {
+    
+    private final int[] values;
+    private int head; //point to first element of queue
+    private int tail; //point to the element after last
+    private int elements;
+        
+    public Queue(int size) {
+        values = new int[size];
+    }
+    
+    public void append(int value) {
+        values[tail] = value;
+        tail = (tail + 1) % values.length;
+        elements++;
+    }
+    
+    public int get() {
+        int v = values[head];
+        head = (head + 1) % values.length;
+        elements--;
+        return v;
+    }
+    
+    public boolean isEmpty(){
+        return elements == 0;
+    }
+    
+    public boolean isFull() {
+        return elements == values.length;
+    }    
+}
